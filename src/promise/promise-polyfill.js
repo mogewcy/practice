7d2b6cf2a8aabb7c3e8d7ff5aabb10a7e834e040
浴火小青春 <10253976+mogewcy@users.noreply.github.com>
2021-02-22 10:39:53 +0800

    promise-polyfill

--- src/promise/promise-polyfill.js
+++ src/promise/promise-polyfill.js
@@ -2,7 +2,56 @@ const PENDING = "PENDING";
 const FULFILLED = "FULFILLED";
 const REJECTED = "REJECTED";
 
-const resolvePromise = (promise2, x, resolve, reject) => {};
+// resolvePromise是为了在then的回调中完整的返回一个promise对象
+const resolvePromise = (promise2, x, resolve, reject) => {
+  // 自己等待自己完成是错误的实现，用一个类型错误，结束掉 promise  Promise/A+ 2.3.1
+  if (promise2 === x) {
+    return reject(
+      new TypeError("Chaining cycle detected for promise #<Promise>")
+    );
+  }
+
+  // Promise/A+ 2.3.3.3.3 只能调用一次
+  let called;
+  // 后续的条件要严格判断 保证代码能和别的库一起使用, x是对象或者函数
+  if ((typeof x === "object" && x != null) || typeof x === "function") {
+    try {
+      // 为了判断 resolve 过的就不用再 reject 了（比如 reject 和 resolve 同时调用的时候）  Promise/A+ 2.3.3.1
+      let then = x.then;
+      if (typeof then === "function") {
+        // 2.3.3.3 x.then为函数的话，把x当成this调用
+        // 不要写成 x.then，直接 then.call 就可以了 因为 x.then 会再次取值，Object.defineProperty  Promise/A+ 2.3.3.3
+        then.call(
+          x,
+          (y) => {
+            // 根据 promise 的状态决定是成功还是失败
+            if (called) return;
+            called = true;
+            // 递归解析的过程（因为可能 promise 中还有 promise） Promise/A+ 2.3.3.3.1
+            resolvePromise(promise2, y, resolve, reject);
+          },
+          (r) => {
+            // 只要失败就失败 Promise/A+ 2.3.3.3.2
+            if (called) return;
+            called = true;
+            reject(r);
+          }
+        );
+      } else {
+        // 如果 x.then 是个普通值就直接返回 resolve 作为结果  Promise/A+ 2.3.3.4
+        resolve(x);
+      }
+    } catch (e) {
+      // Promise/A+ 2.3.3.2
+      if (called) return;
+      called = true;
+      reject(e);
+    }
+  } else {
+    // 如果 x 是个普通值就直接返回 resolve 作为结果  Promise/A+ 2.3.4
+    resolve(x);
+  }
+};
 
 class Promise {
   constructor(executor) {
@@ -45,8 +94,10 @@ class Promise {
   }
 
   then(onFulfilled, onRejected) {
+    // 如果没传onFulfilled的话，要透传值, Promise.resolve(1).then().then(console.log) 打印出来1
     onFulfilled = typeof onFulfilled === "function" ? onFulfilled : (v) => v;
 
+    // 如果没传onRejected的话，要透错误, Promise.reject(1).catch().catch(console.log) 打印出来1
     onRejected =
       typeof onRejected === "function"
         ? onRejected
@@ -57,6 +108,7 @@ class Promise {
     let promise2 = new Promise((resolve, reject) => {
       if (this.status === FULFILLED) {
         setTimeout(() => {
+          // setTimeout是为了模仿异步行为，我们知道setTimoue回调是会被添加到宏任务队列中，而promise.then回调是添加到微任务队列当中，所以要完全模拟这一行为的话，需要改写为queueMicrotask或者mutaionObserver来执行
           try {
             let x = onFulfilled(this.value);
 
